bef96d696d925d3c93610880348e7df0158506dd
Dave Gomez <davegomez426@gmail.com>
2024-08-26 16:40:05 -0400

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by David Gomez on 2024-08-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
